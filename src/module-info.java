d0a75415f12fbc4e24114d884bbb22d11782e737
AmbiNtsoah <ambinints04@gmail.com>
2025-03-19 14:00:36 +0300

    Update module-info

--- src/module-info.java
+++ src/module-info.java
@@ -6,9 +6,9 @@
  */
 module Project_Authentification {
 	requires java.desktop;
-<<<<<<< HEAD
-	requires org.junit.jupiter.api;
-=======
+
+	// requires org.junit.jupiter.api;
+
 	requires java.sql;
->>>>>>> 935d8e7ce7cd6399ad72e5d586815bb49d777390
+
 }
